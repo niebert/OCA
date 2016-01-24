659a70aa7cc754750411bb77a5001095e8a0ec0f
Bert Niehaus <niehausbert@gmail.com>
2016-01-24 17:03:48 +0100

    MindMap first Version added

---
+++ templates/FreeMind/EasyChair_Paper_MindMap.mm
@@ -0,0 +1,35 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453651155223" ID="ID_1480839588" MODIFIED="1453651290057" TEXT="EasyChair Paper MindMap">
+<node CREATED="1453651189600" ID="ID_10818205" MODIFIED="1453651239113" POSITION="right" TEXT="Abstract">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is the abstract of the paper. Try to export the mindmap to LibreOffice.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453651240454" ID="ID_956022918" MODIFIED="1453651243916" POSITION="left" TEXT="Introduction"/>
+<node CREATED="1453651291389" ID="ID_390726899" MODIFIED="1453651387923" POSITION="right" TEXT="Methodology">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Use e.g. http://dictation.io to record a few comments and ideas for this mindmap.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453651298288" ID="ID_1293767810" MODIFIED="1453651303988" POSITION="left" TEXT="Results"/>
+<node CREATED="1453651305573" ID="ID_1716523151" MODIFIED="1453651309604" POSITION="right" TEXT="Conclusions"/>
+<node CREATED="1453651311029" ID="ID_1929314012" MODIFIED="1453651317115" POSITION="left" TEXT="Acknowledgements"/>
+</node>
+</map>
