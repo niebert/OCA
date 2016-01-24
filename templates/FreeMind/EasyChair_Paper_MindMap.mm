699c738365804c952847af4b4c8a3648a4ba85d9
Bert Niehaus <niehausbert@gmail.com>
2016-01-24 20:43:01 +0100

    Updated Freemind Mindmap

--- templates/FreeMind/EasyChair_Paper_MindMap.mm
+++ templates/FreeMind/EasyChair_Paper_MindMap.mm
@@ -1,35 +1,96 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1453651155223" ID="ID_1480839588" MODIFIED="1453651290057" TEXT="EasyChair Paper MindMap">
-<node CREATED="1453651189600" ID="ID_10818205" MODIFIED="1453651239113" POSITION="right" TEXT="Abstract">
+<node CREATED="1453651155223" ID="ID_1480839588" MODIFIED="1453664046998" TEXT="EasyChair Paper MindMap">
+<node CREATED="1453651189600" ID="ID_10818205" MODIFIED="1453664236620" POSITION="right" TEXT="Abstract">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      This is the abstract of the paper. Try to export the mindmap to LibreOffice.
+      This is the abstract of the paper. Try to export the mindmap in Freemind to Open-Office-Writer format and edit&#160;exported ODT file in&#160;LibreOffice.
     </p>
   </body>
 </html>
 </richcontent>
 </node>
-<node CREATED="1453651240454" ID="ID_956022918" MODIFIED="1453651243916" POSITION="left" TEXT="Introduction"/>
-<node CREATED="1453651291389" ID="ID_390726899" MODIFIED="1453651387923" POSITION="right" TEXT="Methodology">
+<node CREATED="1453651240454" ID="ID_956022918" MODIFIED="1453663865666" POSITION="left" TEXT="Introduction">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Use e.g. http://dictation.io to record a few comments and ideas for this mindmap.
+      Explain how this paper contributes to risk mitigation and capacity building or to collaborative mapping.
     </p>
   </body>
 </html>
 </richcontent>
 </node>
-<node CREATED="1453651298288" ID="ID_1293767810" MODIFIED="1453651303988" POSITION="left" TEXT="Results"/>
-<node CREATED="1453651305573" ID="ID_1716523151" MODIFIED="1453651309604" POSITION="right" TEXT="Conclusions"/>
-<node CREATED="1453651311029" ID="ID_1929314012" MODIFIED="1453651317115" POSITION="left" TEXT="Acknowledgements"/>
+<node CREATED="1453651291389" ID="ID_390726899" MODIFIED="1453664035799" POSITION="right" TEXT="Methodology">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Use e.g. http://dictation.io to record a few comments and ideas for this mindmap. H
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453651298288" ID="ID_1293767810" MODIFIED="1453663925610" POSITION="left" TEXT="Results">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      When you apply your proposed methodology, you demostrate the results of your applied methodology.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453651305573" ID="ID_1716523151" MODIFIED="1453663962769" POSITION="right" TEXT="Conclusions">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Conclude recommendation for risk mitigation or collabortive mapping.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453651311029" ID="ID_1929314012" MODIFIED="1453664010567" POSITION="left" TEXT="Acknowledgements">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Thanks to ...?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1453664049633" ID="ID_163034610" MODIFIED="1453664146457" POSITION="right" TEXT="Open Source / Content / Data">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Explain how did you used or recommend to use Open Source, Open Content or Open Geo Data.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </map>
